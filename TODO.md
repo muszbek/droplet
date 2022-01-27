eda14f98750bd28423c51bdb2f66eacf5fd0edfd
muszbek <muszbektamas@gmail.com>
2022-01-27 14:17:51 +0100

    setting up couchdb

--- TODO.md
+++ TODO.md
@@ -1,9 +1,6 @@
-- remove ecto references from phoenix so it compiles
 - docker-compose file for couchdb
 - bash script for couchdb migrations
 - set up couchdb connection in phoenix
 - unit test framework for nosql couchdb (no ecto)
 ...
 OAUTH2
-
-docker run -e COUCHDB_USER=droplet -e COUCHDB_PASSWORD=droplet --name couchdb couchdb:3.2.1
---
+++ droplet_store/laucher.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source ../couchdb/couchdb.env
+# Wait until CouchDB is ready
+while ! curl --silent --fail -o /dev/null $COUCHDB_HOST:5984/
+do
+    echo "$(date) - waiting for database to start..."
+    sleep 2
+done
+
+DB_ADDRESS=http://$COUCHDB_USER:$COUCHDB_PASSWORD@$COUCHDB_HOST:5984
+
+if ! curl -s -f -o /dev/null $DB_ADDRESS/_users; then
+    echo "$(date) - adding _users database..."
+    curl -X PUT -s -o /dev/null $DB_ADDRESS/_users
+fi
+
+if ! curl -s -f -o /dev/null $DB_ADDRESS/droplet; then
+    echo "$(date) - adding droplet database..."
+    curl -X PUT -s -o /dev/null $DB_ADDRESS/droplet
+fi
+
+exec mix phx.server

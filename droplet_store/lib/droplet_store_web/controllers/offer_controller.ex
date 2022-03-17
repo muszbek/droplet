d535db2ac941d687e8ed02765bb1a537de0fb80a
muszbek <muszbektamas@gmail.com>
2022-03-17 19:34:42 +0100

    most controller tests pass for offer context

--- droplet_store/lib/droplet_store_web/controllers/offer_controller.ex
+++ droplet_store/lib/droplet_store_web/controllers/offer_controller.ex
@@ -4,59 +4,59 @@ defmodule DropletStoreWeb.OfferController do
   alias DropletStore.Publishments
   alias DropletStore.Publishments.Offer
 
-  def index(conn, _params) do
+  def index(conn, %{"store_id" => store_id}) do
     offers = Publishments.list_offers()
-    render(conn, "index.html", offers: offers)
+    render(conn, "index.html", store_id: store_id, offers: offers)
   end
 
-  def new(conn, _params) do
+  def new(conn, %{"store_id" => store_id}) do
     changeset = Publishments.change_offer(%Offer{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, "new.html", store_id: store_id, changeset: changeset)
   end
 
-  def create(conn, %{"offer" => offer_params}) do
+  def create(conn, %{"store_id" => store_id, "offer" => offer_params}) do
     case Publishments.create_offer(offer_params) do
       {:ok, offer} ->
         conn
         |> put_flash(:info, "Offer created successfully.")
-        |> redirect(to: Routes.offer_path(conn, :show, offer))
+        |> redirect(to: Routes.store_offer_path(conn, :show, store_id, offer))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render(conn, "new.html", store_id: store_id, changeset: changeset)
     end
   end
 
-  def show(conn, %{"id" => id}) do
+  def show(conn, %{"store_id" => store_id, "id" => id}) do
     offer = Publishments.get_offer!(id)
-    render(conn, "show.html", offer: offer)
+    render(conn, "show.html", store_id: store_id, offer: offer)
   end
 
-  def edit(conn, %{"id" => id}) do
+  def edit(conn, %{"store_id" => store_id, "id" => id}) do
     offer = Publishments.get_offer!(id)
     changeset = Publishments.change_offer(offer)
-    render(conn, "edit.html", offer: offer, changeset: changeset)
+    render(conn, "edit.html", store_id: store_id, offer: offer, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "offer" => offer_params}) do
+  def update(conn, %{"store_id" => store_id, "id" => id, "offer" => offer_params}) do
     offer = Publishments.get_offer!(id)
 
     case Publishments.update_offer(offer, offer_params) do
       {:ok, offer} ->
         conn
         |> put_flash(:info, "Offer updated successfully.")
-        |> redirect(to: Routes.offer_path(conn, :show, offer))
+        |> redirect(to: Routes.store_offer_path(conn, :show, store_id, offer))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", offer: offer, changeset: changeset)
+        render(conn, "edit.html", store_id: store_id, offer: offer, changeset: changeset)
     end
   end
 
-  def delete(conn, %{"id" => id}) do
+  def delete(conn, %{"store_id" => store_id, "id" => id}) do
     offer = Publishments.get_offer!(id)
     {:ok, _offer} = Publishments.delete_offer(offer)
 
     conn
     |> put_flash(:info, "Offer deleted successfully.")
-    |> redirect(to: Routes.offer_path(conn, :index))
+    |> redirect(to: Routes.store_offer_path(conn, :index, store_id))
   end
 end
--- droplet_store/test/droplet_store_web/controllers/offer_controller_test.exs
+++ droplet_store/test/droplet_store_web/controllers/offer_controller_test.exs
@@ -3,37 +3,39 @@ defmodule DropletStoreWeb.OfferControllerTest do
 
   import DropletStore.PublishmentsFixtures
 
-  @create_attrs %{currency: "some currency", description: "some description", price: "120.5", title: "some title"}
-  @update_attrs %{currency: "some updated currency", description: "some updated description", price: "456.7", title: "some updated title"}
+  @create_attrs %{currency: "EUR", description: "some description", price: "120.5", title: "some title"}
+  @update_attrs %{description: "some updated description", price: "456.7", title: "some updated title"}
   @invalid_attrs %{currency: nil, description: nil, price: nil, title: nil}
 
+  setup [:register_and_log_in_user, :create_store]
+  
   describe "index" do
-    test "lists all offers", %{conn: conn} do
-      conn = get(conn, Routes.offer_path(conn, :index))
+    test "lists all offers", %{conn: conn, store: store} do
+      conn = get(conn, Routes.store_offer_path(conn, :index, store.id))
       assert html_response(conn, 200) =~ "Listing Offers"
     end
   end
 
   describe "new offer" do
-    test "renders form", %{conn: conn} do
-      conn = get(conn, Routes.offer_path(conn, :new))
+    test "renders form", %{conn: conn, store: store} do
+      conn = get(conn, Routes.store_offer_path(conn, :new, store.id))
       assert html_response(conn, 200) =~ "New Offer"
     end
   end
 
   describe "create offer" do
-    test "redirects to show when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.offer_path(conn, :create), offer: @create_attrs)
+    test "redirects to show when data is valid", %{conn: conn, store: store} do
+      conn = post(conn, Routes.store_offer_path(conn, :create, store.id), offer: @create_attrs)
 
       assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == Routes.offer_path(conn, :show, id)
+      assert redirected_to(conn) == Routes.store_offer_path(conn, :show, store.id, id)
 
-      conn = get(conn, Routes.offer_path(conn, :show, id))
+      conn = get(conn, Routes.store_offer_path(conn, :show, store.id, id))
       assert html_response(conn, 200) =~ "Show Offer"
     end
 
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.offer_path(conn, :create), offer: @invalid_attrs)
+    test "renders errors when data is invalid", %{conn: conn, store: store} do
+      conn = post(conn, Routes.store_offer_path(conn, :create, store.id), offer: @invalid_attrs)
       assert html_response(conn, 200) =~ "New Offer"
     end
   end
@@ -41,8 +43,8 @@ defmodule DropletStoreWeb.OfferControllerTest do
   describe "edit offer" do
     setup [:create_offer]
 
-    test "renders form for editing chosen offer", %{conn: conn, offer: offer} do
-      conn = get(conn, Routes.offer_path(conn, :edit, offer))
+    test "renders form for editing chosen offer", %{conn: conn, store: store, offer: offer} do
+      conn = get(conn, Routes.store_offer_path(conn, :edit, store.id, offer))
       assert html_response(conn, 200) =~ "Edit Offer"
     end
   end
@@ -50,16 +52,16 @@ defmodule DropletStoreWeb.OfferControllerTest do
   describe "update offer" do
     setup [:create_offer]
 
-    test "redirects when data is valid", %{conn: conn, offer: offer} do
-      conn = put(conn, Routes.offer_path(conn, :update, offer), offer: @update_attrs)
-      assert redirected_to(conn) == Routes.offer_path(conn, :show, offer)
+    test "redirects when data is valid", %{conn: conn, store: store, offer: offer} do
+      conn = put(conn, Routes.store_offer_path(conn, :update, store.id, offer), offer: @update_attrs)
+      assert redirected_to(conn) == Routes.store_offer_path(conn, :show, store.id, offer)
 
-      conn = get(conn, Routes.offer_path(conn, :show, offer))
-      assert html_response(conn, 200) =~ "some updated currency"
+      conn = get(conn, Routes.store_offer_path(conn, :show, store.id, offer))
+      assert html_response(conn, 200) =~ "some updated description"
     end
 
-    test "renders errors when data is invalid", %{conn: conn, offer: offer} do
-      conn = put(conn, Routes.offer_path(conn, :update, offer), offer: @invalid_attrs)
+    test "renders errors when data is invalid", %{conn: conn, store: store, offer: offer} do
+      conn = put(conn, Routes.store_offer_path(conn, :update, store.id, offer), offer: @invalid_attrs)
       assert html_response(conn, 200) =~ "Edit Offer"
     end
   end
@@ -67,16 +69,21 @@ defmodule DropletStoreWeb.OfferControllerTest do
   describe "delete offer" do
     setup [:create_offer]
 
-    test "deletes chosen offer", %{conn: conn, offer: offer} do
-      conn = delete(conn, Routes.offer_path(conn, :delete, offer))
-      assert redirected_to(conn) == Routes.offer_path(conn, :index)
+    test "deletes chosen offer", %{conn: conn, store: store, offer: offer} do
+      conn = delete(conn, Routes.store_offer_path(conn, :delete, store.id, offer))
+      assert redirected_to(conn) == Routes.store_offer_path(conn, :index, store.id)
 
       assert_error_sent 404, fn ->
-        get(conn, Routes.offer_path(conn, :show, offer))
+        get(conn, Routes.store_offer_path(conn, :show, store.id, offer))
       end
     end
   end
 
+  defp create_store(%{conn: conn}) do
+    store = DropletStore.SubscriptionsFixtures.store_fixture()
+    %{conn: conn, store: store}
+  end
+  
   defp create_offer(_) do
     offer = offer_fixture()
     %{offer: offer}
